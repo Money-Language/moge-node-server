3ea9beb482a2601c40ec283436371cfe97caecfd
seungwanRyu01 <seungwan6255@gmail.com>
2022-11-02 00:12:47 +0900

    feat : 구현중....

--- src/app/Board/boardService.js
+++ src/app/Board/boardService.js
@@ -51,12 +51,47 @@ exports.updateUserPoint = async function (quizIdx, userIdx) {
     }
 }
 
+// // 게시글 + 퀴즈 + 객관식/주관식 정답 등록
+// exports.createBoardQuiz = async ( title, userIdx, categoryIdx, quizType, question, hint, answerSelectIdx, answer ) => {
+//     try {
+//         const connection = await pool.getConnection(async (conn) => (conn));
+//         const createBoardResponse = await boardDao.createBoard(connection, title, userIdx, categoryIdx);
+//         const boardIdx = createBoardResponse.insertId
+
+//         for (let quizIter of question) {
+//             const createQuizResponse = await boardDao.createQuiz(connection, quizIter, quizType, boardIdx);
+//             const quizIdx = createQuizResponse.insertId
+//             console.log(`추가된 퀴즈 문제 인덱스 번호 : ${createQuizResponse.insertId}`);
+
+//             if (quizType == "객관식") {
+//                 for (objectiveIter of answer) {
+//                     const createObjectiveAnswerResponse = await boardDao.createObjectiveAnswer(connection, objectiveIter, answerSelectIdx, quizIdx);
+//                     console.log(`추가된 객관식 정답 인덱스 번호 : ${createObjectiveAnswerResponse.insertId}`);
+//                 }
+//             } else {
+//                 for (subjectiveIter of answer) {
+//                     const createSubjectiveAnswerResponse = await boardDao.createSubjectiveAnswer(connection, hint, answerSelectIdx, subjectiveIter, quizIdx);
+//                     console.log(`추가된 주관식 정답 인덱스 번호 : ${createSubjectiveAnswerResponse.insertId}`);
+//                 }
+//             }
+//         }
+//         const result = { userIdx, categoryIdx, title, quizType, question, hint, answerSelectIdx, answer }
+//         connection.release();
+//         return response(baseResponse.SUCCESS, result)
+//     } catch (err) {
+//         logger.error(`App - createBoardQuiz Service error\n: ${err.message}`);
+//         return errResponse(baseResponse.DB_ERROR);
+//     }
+// }
+
+
 // 게시글 + 퀴즈 + 객관식/주관식 정답 등록
-exports.createBoardQuiz = async ( categoryIdx, title, userIdx, quizType, question, hint, answerSelectIdx, answer ) => {
+exports.createBoardQuiz = async ( title, userIdx, categoryIdx, quizType, question, answer, answerSelectIdx ) => {
     try {
         const connection = await pool.getConnection(async (conn) => (conn));
         const createBoardResponse = await boardDao.createBoard(connection, title, userIdx, categoryIdx);
         const boardIdx = createBoardResponse.insertId
+        console.log("삽입된 게시글 인덱스 번호 : " , boardIdx)
 
         for (let quizIter of question) {
             const createQuizResponse = await boardDao.createQuiz(connection, quizIter, quizType, boardIdx);
